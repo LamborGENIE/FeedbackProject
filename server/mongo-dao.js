3a3754c5f40d0c3207801d852e544605ec13d10e
Kemin Liu <kjiangli@travelers.com>
2023-10-11 13:07:38 -0400

    Added more CRUD functions

--- server/mongo-dao.js
+++ server/mongo-dao.js
@@ -7,11 +7,22 @@ const messageCollectionName = 'message';
 // employee and message collections
 
 // Create Employee
-async function createEmployeeDocument() {
+async function createEmployeeDocument(employee_id, first, last, manager_id) {
   const client = await MongoClient.connect(url);
   const db = client.db(dbName);
   const collection = db.collection(employeeCollectionName);
-  const result = await collection.insertOne({employeeID : 2, firstname: "Benton", lastname: "Snodgrass", role: "Employee", managerID: 4});
+  const result = await collection.insertOne({employeeID : employee_id, firstname: first, lastname: last, role: "Employee", managerID: manager_id});
+  console.log(`New document created with _id: ${result.insertedId}`);
+  client.close();
+  return result.insertedId;
+}
+
+// Create Manager
+async function createManagerDocument(employee_id, first, last, reports) {
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection(employeeCollectionName);
+  const result = await collection.insertOne({employeeID : employee_id, firstname: first, lastname: last, role: "Manager", managerID: reports});
   console.log(`New document created with _id: ${result.insertedId}`);
   client.close();
   return result.insertedId;
@@ -28,11 +39,46 @@ async function createMessageDocument() {
   return result.insertedId;
 }
 
+//Read One Employee Doc
+async function readOneEmployeeDocument(id){
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection(employeeCollectionName);
+  const document = await collection.findOne({employeeID: id});
+  console.log(document);
+  client.close();
+  return document
+}
+
+//Read One Message Doc
+async function readOneMessageDocument(id){
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection(messageCollectionName);
+  const document = await collection.findOne({messageID: id});
+  console.log(document);
+  client.close();
+  return document
+}
+
+//Read All of an Employee's Messages
+async function readEmployeeAllMessages(employee_id){
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection(messageCollectionName);
+  const documents = await collection.find({from : employee_id}).toArray();
+  console.log(documents);
+  client.close();
+  return documents;
+}
+
 // USAGE
 async function main() {
-  let insertedId = await createMessageDocument();
+  //let insertedId = await createEmployeeDocument(3,"Ola", "Ekdahl", 2);
+  let insertedId = await createManagerDocument(4,"Rap", "Payne", [1,2,3]);
   // await readDocuments();
-  // await readOneDocument(insertedId);
+  // await readOneMessageDocument(2);
+  // await readEmployeeAllMessages(1)
   // await updateDocument(insertedId, { age: 35 });
   // await readDocuments();
   // await deleteDocument(insertedId);
