b5c5be4f058a2b70c78f64e4987878549651be62
Kemin Liu <kjiangli@travelers.com>
2023-10-11 11:24:37 -0400

    Allow mongo-dao to access the database

--- server/mongo-dao.js
+++ server/mongo-dao.js
@@ -1,11 +1,42 @@
-const mongodb = require("mongodb"); // mongo client library
-const url = "mongodb://localhost:27017/feedback_db";
-let dbPool;
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://127.0.0.1:27017";
+const dbName = 'feedback_db';
+const employeeCollectionName = 'employee';
+const messageCollectionName = 'message';
+
 // employee and message collections
-mongodb.MongoClient.connect(url, function (err, db) {
-  if (!err) {
-    dbPool = db.db("feedback_db");
-  } else {
-    console.log("DB CONNECTION FAILED. Is database running?");
-  }
-});
+
+// Create Employee
+async function createEmployeeDocument() {
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection(employeeCollectionName);
+  const result = await collection.insertOne({employeeID : 2, firstname: "Benton", lastname: "Snodgrass", role: "Employee", managerID: 4});
+  console.log(`New document created with _id: ${result.insertedId}`);
+  client.close();
+  return result.insertedId;
+}
+
+//Create Message
+async function createMessageDocument() {
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection(messageCollectionName);
+  const result = await collection.insertOne({messageID: 2, from: 2, to: 1, message: "I'm quitting", sentiment: "Negative"});
+  console.log(`New document created with _id: ${result.insertedId}`);
+  client.close();
+  return result.insertedId;
+}
+
+// USAGE
+async function main() {
+  let insertedId = await createMessageDocument();
+  // await readDocuments();
+  // await readOneDocument(insertedId);
+  // await updateDocument(insertedId, { age: 35 });
+  // await readDocuments();
+  // await deleteDocument(insertedId);
+  // await readDocuments();
+}
+
+main();
