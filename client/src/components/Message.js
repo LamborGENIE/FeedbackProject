c3c15072748f810a125051c21d98fc152f504c3f
bentonsnodgrass <109827202+bentonsnodgrass@users.noreply.github.com>
2023-10-12 08:34:32 -0400

    added message.js

---
+++ client/src/components/Message.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+function ViewMessage() {
+
+    const [message, setMessage] = useState([]);
+
+    const getMessage = function() {
+        fetch(`http://127.0.0.1:3001/api/message/3`)
+        .then(response => response.json())
+        .then(data => {
+            setMessage(data)
+            console.log(data)
+        })
+        .catch(err => {console.log(err)})
+    }
+
+    
+    useEffect(() => {getMessage()}, []);
+    return(
+        <>
+        <h3>Message</h3>
+        <div >
+        {message.message}
+        </div>
+        <h3>Response</h3>
+        <div >
+        {message.response}
+        </div>
+        
+        </>
+        
+    );
+
+
+}
+
+export default ViewMessage;
